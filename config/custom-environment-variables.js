b86b5123f580c690468f8efe16b33d538ac74d5f
Pawan Sasanka <pawan@duosoftware.com>
2015-08-03 16:29:36 +0530

    Config updated

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -3,7 +3,7 @@ module.exports = {
     "Type":"SYS_DATABASE_TYPE",
     "User":"SYS_DATABASE_POSTGRES_USER",
     "Password":"SYS_DATABASE_POSTGRES_PASSWORD",
-    "Port":"SYS_SQL_POR",
+    "Port":"SYS_SQL_PORT",
     "Host":"SYS_DATABASE_HOST",
     "Database":"SYS_DATABASE_POSTGRES_USER"
   },
